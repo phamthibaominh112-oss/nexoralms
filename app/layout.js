1895f42cf4de87bf9802ecfc278775d5f8682715
Meimei Bae <minhpham@Mac.lan>
2026-07-18 20:01:16 +0700

    Add favicon

--- app/layout.js
+++ app/layout.js
@@ -12,3 +12,23 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
+import "./globals.css";
+
+export const metadata = {
+  title: "Nexora | Road to IELTS 8.0",
+  description: "Road to IELTS 8.0",
+
+  icons: {
+    icon: "/favicon.jpeg",
+    shortcut: "/favicon.jpeg",
+    apple: "/favicon.jpeg",
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
